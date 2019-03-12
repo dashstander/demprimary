af73fc9a5d8a6022c77cdc4492b166ed4f3121d2
Dashiell <dash.stander@gmail.com>
2019-03-12 00:59:27 -0700

    Add app_name to config.yml, fix up scrape_tweets, add util.R file

--- R/scrape.R
+++ R/scrape.R
@@ -1,10 +1,10 @@
-library(rtweet)
 library(readr)
-library(dplyr)
 library(futile.logger)
 
+source("R/util.R")
+
 token = create_token(
-  app = "dem_primary_nlp",
+  app = config::get("app_name"),
   consumer_key = config::get("consumer_api_key"),
   consumer_secret = config::get("consumer_api_secret"),
   access_token = config::get("access_token"),
@@ -13,30 +13,33 @@ token = create_token(
 flog.logger("scraper", INFO, 
             appender = appender.file(paste0("logs/", as.character(Sys.Date()), ".log")))
 
-paginate_tweets <- function(search_term, num_tweets, min_status_id = NULL) {
-  search_tweets(search_term, 
-                n = num_tweets, 
-                max_id = min_status_id,
-                includs_rts = FALSE,
-                retryonratelimit = TRUE)
-}
 
+###########################################################
+#' Main function to grab a candidate's tweets
+#' 
 scrape_tweets <- function(candidate, n, step, output_csv, logger = "") {
   
   tweets_returned = 0
-  min_status_id = NULL
-  
+  if (file.exists(output_csv)) {
+    min_status_id = read_csv(output_csv) %>% pull(status_id) %>% min
+  } else {
+    min_status_id = NULL
+  }
   flog.info(sprintf("Starting scrape job at %s", as.character(Sys.time())),
             name= logger)
   all_tweets = list()
   i = 1
   while (tweets_returned < n) {
     tweets = paginate_tweets(candidate, step, min_status_id)
+    num_tweets = nrow(tweets)
+    
+    if (num_tweets == 0) break
+    
     write_csv(tweets, output_csv, append = file.exists(output_csv))
-    tweets_returned = tweets_returned + nrow(tweets)
+    tweets_returned = tweets_returned + num_tweets
     min_status_id = min(tweets$status_id)
     flog.info(sprintf("Canidate %s: %d tweets for a total of %d. Earliest status_id: %s for time %s",
-                      candidate, nrow(tweets), tweets_returned, min_status_id, as.character(min(tweets$created_at))))
+                      candidate, num_tweets, tweets_returned, min_status_id, as.character(min(tweets$created_at))))
     all_tweets[i] = tweets
     i = i + 1
   }
@@ -50,7 +53,13 @@ candidates = PrimaryField %>%
   mutate(full_name = paste(first_name, last_name)) %>% 
   pull(full_name) %>% tolower
 
-
+#map(candidates,
+#    ~scrape_tweets(.,
+#                  250000,
+#                  5000,
+#                  paste0("data/", ., "_", as.character(Sys.Date()), ".csv"),
+#                  "scraper"),
+#    )
 
 
 
---
+++ R/util.R
@@ -0,0 +1,36 @@
+library(purrr)
+library(dplyr)
+library(rtweet)
+
+
+###########################################################
+#' Transforms twitter geographic coordinates to strings
+#' @description geo-coordinate list columns transformed to strings, which can be
+#' written to CSV
+#' @param coords: list, the list of coordinates
+#' @return a ";" separate string with all of the coordinates
+geo_coords_to_string <- function(coords) {
+  empty_or_collapse <- function(row) {
+    row = unlist(row)
+    if (any(is.na(row))) {
+      ""
+    } else {
+      paste(row, collapse = "; ")
+    }
+  }
+  purrr::map_chr(coords, empty_or_collapse)
+}
+
+
+
+###########################################################
+#' One call to search tweets
+#'
+paginate_tweets <- function(search_term, num_tweets, min_status_id = NULL) {
+  search_tweets(search_term, 
+                n = num_tweets, 
+                max_id = min_status_id,
+                includs_rts = FALSE,
+                retryonratelimit = TRUE) %>% 
+    mutate_if(is.list, geo_coords_to_string)
+}
